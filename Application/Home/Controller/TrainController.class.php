2becbc8f189f82d76b84c47e8ff67d86980d580a
Liu jinshuai <ljs_0829@qq.com>
2016-10-21 19:53:48 +0800

    feat: 新增课程，培训，关于我们，须知模块

--- Application/Home/Controller/TrainController.class.php
+++ Application/Home/Controller/TrainController.class.php
@@ -7,7 +7,7 @@
 namespace Home\Controller;
 use Common\Controller\BaseController;
 use Think\Controller;
-class Train extends BaseController {
+class TrainController extends BaseController {
     public function index(){
         $this->display();
     }
